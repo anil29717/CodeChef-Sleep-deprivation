d0d92bf39046f34c887de63a82a48a256600aeb6
Anil Kumar <93667898+anil29717@users.noreply.github.com>
2022-09-29 07:52:59 +0530

    Add files via upload

---
+++ SLEEP.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    if(x<7)
+	    cout<<"YES";
+	    
+	    else
+	    cout<<"NO";
+	}
+	return 0;
+}
